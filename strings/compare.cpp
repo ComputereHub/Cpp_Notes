36fd2409c1df927cb35a4fea0f3d0697c7dd5bd6
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-13 16:38:06 +0530

    Create compare.cpp

---
+++ strings/compare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    if(s1 == s2)
+        cout << "Strings are Equal";
+    else
+        cout << "Strings are Not Equal";
+
+    return 0;
+}
