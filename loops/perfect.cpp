055338137e680ada7efe30f52be5fdf228ea7457
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-17 21:26:55 +0530

    Create perfect.cpp

---
+++ loops/perfect.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n=6,sum=0;
+    for(int i=1;i<n;i++){
+        if(n%i==0) sum+=i;
+    }
+    if(sum==n) cout<<"Perfect Number";
+    else cout<<"Not Perfect";
+    return 0;
+}
