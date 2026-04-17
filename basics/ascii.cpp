317e90d1603e9e3d77df50fc2d7ef54ac4d5e899
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-17 20:57:20 +0530

    Create ascii.cpp

---
+++ basics/ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter character: ";
+    cin >> ch;
+
+    cout << "ASCII Value = " << (int)ch;
+
+    return 0;
+}
