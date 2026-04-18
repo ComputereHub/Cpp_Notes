2245fdce579f74b8812a94995c0373d9465952fc
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-18 11:56:26 +0530

    Update leap-year.cpp

--- basics/leap-year.cpp
+++ basics/leap-year.cpp
@@ -6,7 +6,11 @@ int main() {
     cout << "Enter year: ";
     cin >> year;
 
-    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    if(year % 400 == 0)
+        cout << "Leap Year";
+    else if(year % 100 == 0)
+        cout << "Not Leap Year";
+    else if(year % 4 == 0)
         cout << "Leap Year";
     else
         cout << "Not Leap Year";
