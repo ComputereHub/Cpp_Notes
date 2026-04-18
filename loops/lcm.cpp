fb1263c103700390f9488d3830caf0a51c1022d9
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-18 12:02:24 +0530

    Update lcm.cpp

--- loops/lcm.cpp
+++ loops/lcm.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a=12,b=18;
-    int max = (a>b)?a:b;
-    while(true){
-        if(max%a==0 && max%b==0){
-            cout<<"LCM = "<<max;
-            break;
-        }
-        max++;
+    int a = 12, b = 18;
+    int lcm;
+
+    // बड़े नंबर से शुरू करें
+    lcm = (a > b) ? a : b;
+
+    while(lcm % a != 0 || lcm % b != 0) {
+        lcm++;   // अगला नंबर check करें
     }
+
+    cout << "LCM = " << lcm;
+
     return 0;
 }
