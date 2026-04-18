08e77671032860081e71a13567d24683a977340a
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-18 11:58:41 +0530

    Update armstrong.cpp

--- loops/armstrong.cpp
+++ loops/armstrong.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n=153,sum=0,temp=n;
-    while(temp>0){
-        int d=temp%10;
-        sum+=d*d*d;
-        temp/=10;
+    int num = 153;
+    int sum = 0;
+    int digit;
+
+    while(num > 0) {
+        digit = num % 10;        // last digit निकाला
+        sum = sum + digit*digit*digit;
+        num = num / 10;          // next digit के लिए
     }
-    if(sum==n) cout<<"Armstrong Number";
-    else cout<<"Not Armstrong";
+
+    if(sum == 153)
+        cout << "Armstrong Number";
+    else
+        cout << "Not Armstrong";
+
     return 0;
 }
