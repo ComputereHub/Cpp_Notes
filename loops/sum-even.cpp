b567ca0b3bbb34d21cf846d8ddc5bab00dc08b27
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-18 12:09:46 +0530

    Update sum-even.cpp

--- loops/sum-even.cpp
+++ loops/sum-even.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n=10, sum=0;
-    for(int i=1;i<=n;i++){
-        if(i%2==0) sum+=i;
+    int n = 10;
+    int sum = 0;
+
+    for(int i = 2; i <= n; i += 2) {  // सिर्फ even numbers
+        sum = sum + i;
     }
-    cout<<"Sum of Even Numbers = "<<sum;
+
+    cout << "Sum of Even Numbers = " << sum;
+
     return 0;
 }
