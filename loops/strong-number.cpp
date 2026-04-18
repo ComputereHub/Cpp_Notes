578334505cfb2d89f7d916c434528de708407bd9
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-18 12:00:44 +0530

    Update strong-number.cpp

--- loops/strong-number.cpp
+++ loops/strong-number.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 using namespace std;
-int fact(int n){
-    int f=1;
-    for(int i=1;i<=n;i++) f*=i;
+
+int factorial(int n) {
+    int f = 1;
+    for(int i = 1; i <= n; i++) {
+        f = f * i;
+    }
     return f;
 }
+
 int main() {
-    int n=145,sum=0,temp=n;
-    while(temp>0){
-        sum+=fact(temp%10);
-        temp/=10;
+    int num = 145;
+    int sum = 0;
+    int digit;
+
+    while(num > 0) {
+        digit = num % 10;           // last digit निकाला
+        sum = sum + factorial(digit);
+        num = num / 10;             // next digit के लिए
     }
-    if(sum==n) cout<<"Strong Number";
-    else cout<<"Not Strong";
+
+    if(sum == 145)
+        cout << "Strong Number";
+    else
+        cout << "Not Strong";
+
     return 0;
 }
