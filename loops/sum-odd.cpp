5e43f8df0f3dc3d09162c065f27f606b2277feeb
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-18 12:07:59 +0530

    Update sum-odd.cpp

--- loops/sum-odd.cpp
+++ loops/sum-odd.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n=10, sum=0;
-    for(int i=1;i<=n;i++){
-        if(i%2!=0) sum+=i;
+    int n = 10;
+    int sum = 0;
+
+    for(int i = 1; i <= n; i += 2) {  // सिर्फ odd numbers
+        sum = sum + i;
     }
-    cout<<"Sum of Odd Numbers = "<<sum;
+
+    cout << "Sum of Odd Numbers = " << sum;
+
     return 0;
 }
