728049f5f5693aac994cb7549a1d131b4ef0418b
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-28 16:32:48 +0530

    Create second-largest.cpp

---
+++ arrays/second-largest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10,20,30,40,50};
+    int max=arr[0], second=arr[0];
+
+    for(int i=1;i<5;i++){
+        if(arr[i]>max){
+            second = max;
+            max = arr[i];
+        }
+    }
+
+    cout << "Second Largest = " << second;
+    return 0;
+}
