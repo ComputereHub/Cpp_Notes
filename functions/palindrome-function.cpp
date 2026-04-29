b49117e3eeacec48ca19dcc4fe27da45397870ed
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-29 16:45:32 +0530

    Create palindrome-function.cpp

---
+++ functions/palindrome-function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n){
+    int rev = 0;
+    while(n > 0){
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+    return rev;
+}
+
+int main(){
+    int n = 121;
+
+    if(n == reverseNumber(n))
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+}
