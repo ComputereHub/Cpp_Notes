02f0a2b0bf68f5968c2ffa46989e4f9e910b9e78
Miss Chandan Jain <chandanjain326@gmail.com>
2026-04-29 16:47:35 +0530

    Create even-odd-function.cpp

---
+++ functions/even-odd-function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void checkEvenOdd(int n){
+    if(n % 2 == 0)
+        cout << "Even Number";
+    else
+        cout << "Odd Number";
+}
+
+int main(){
+    checkEvenOdd(4);
+}
