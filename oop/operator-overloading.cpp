b3d8b3baa727323c9995db5c477f9c09cd48ff8a
Miss Chandan Jain <chandanjain326@gmail.com>
2026-05-05 16:50:26 +0530

    Create operator-overloading.cpp

---
+++ oop/operator-overloading.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Demo {
+public:
+    int x;
+
+    Demo(int a) { x = a; }
+
+    Demo operator+(Demo d) {
+        return Demo(x + d.x);
+    }
+};
+
+int main() {
+    Demo d1(5), d2(10);
+    Demo d3 = d1 + d2;
+    cout << d3.x;
+}
