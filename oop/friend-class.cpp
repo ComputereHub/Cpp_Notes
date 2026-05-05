195bf48c2236cdd2d130c5fef458f59d9e479f59
Miss Chandan Jain <chandanjain326@gmail.com>
2026-05-05 16:45:55 +0530

    Create friend-class.cpp

---
+++ oop/friend-class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class A {
+private:
+    int x;
+public:
+    A(){ x = 10; }
+    friend class B;
+};
+
+class B {
+public:
+    void show(A a){
+        cout << a.x;
+    }
+};
+
+int main() {
+    A a;
+    B b;
+    b.show(a);
+}
